efe472a638f1c4de25a9ef878c8e3a07e1c672f9
Wataru Takase <wataru.takase@kek.jp>
2015-12-23 08:29:20 +0900

    Fix if condition

--- nfs_setup.sh
+++ nfs_setup.sh
@@ -6,7 +6,7 @@ mounts="${@}"
 
 for mnt in "${mounts[@]}"; do
   src=$(echo $mnt | awk -F':' '{ print $1 }')
-  if [ -n ${INSECURE} ]; then
+  if [ -n "$INSECURE" ]; then
     insecure=",insecure"
   fi
   echo "$src *(rw,sync,no_subtree_check,fsid=0,no_root_squash${insecure})" >> /etc/exports
