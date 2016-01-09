2651d036390c68e847ce1283dcd4a739d831629c
Wataru Takase <wataru.takase@kek.jp>
2016-01-09 10:58:11 +0900

    Add INSECURE option

--- nfs_setup.sh
+++ nfs_setup.sh
@@ -6,7 +6,10 @@ mounts="${@}"
 
 for mnt in "${mounts[@]}"; do
   src=$(echo $mnt | awk -F':' '{ print $1 }')
-  echo "$src *(rw,sync,no_subtree_check,fsid=0,no_root_squash)" >> /etc/exports
+  if [ -n "$INSECURE" ]; then
+    insecure=",insecure"
+  fi
+  echo "$src *(rw,sync,no_subtree_check,fsid=0,no_root_squash${insecure})" >> /etc/exports
 done
 
 exec runsvdir /etc/sv
